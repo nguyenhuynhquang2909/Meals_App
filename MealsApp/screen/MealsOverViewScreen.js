9ba3fb926f664603447c43da980af1a7ecfa09ad
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-04-21 22:29:55 +0700

    Update navigation

---
+++ MealsApp/screen/MealsOverViewScreen.js
@@ -0,0 +1,16 @@
+import { MEALS} from "../data/dummy-data";
+import { View, Text, StyleSheet } from "react-native";
+
+function MealsOverViewScreen() {
+    return <View style={styles.container}>
+        <Text>Meals Overview Screen</Text>
+    </View>
+}
+export default MealsOverViewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16
+    }
+})
